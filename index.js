48c347647b7fb6a44901cbcf56b2e2a3255ddd71
Avin vij <avinvij26@gmail.com>
2021-05-23 15:27:39 +0530

    added a simple node proxy to help solve cors

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+const { json } = require("express");
+
+app.use(cors());
+app.use(json({}));
+
+app.use("/", (req, res, next) => {
+  let _url = req.body.url;
+  let _method = req.body.method;
+  let _data = req.body.data;
+  let _headers = req.body.headers;
+  //let's make the axios call now.
+  axios({
+    url: _url,
+    method: _method,
+    headers: _headers,
+    data: _data,
+  }).then((response) => {
+    res.status(response.status);
+    res.json(response.data);
+  }).catch(err => {
+    res.status(err?.response?.status || 500);
+    res.json(err?.response?.data || null)
+  })
+})
+
+app.listen(4005, () => {
+  console.log("Proxy server started at port 4005");
+})
